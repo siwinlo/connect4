ed93be8fcce0091be6c81b517b49e61e747c3706
Siwin Lo <siwin.lo@gmail.com>
2019-11-20 16:12:51 -0500

    Updates

---
+++ precache-manifest.f7273090ed22d45f22766584dcdbf465.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeeeb68b8d684927955c6d5c596cd81a",
+    "url": "/connect4/index.html"
+  },
+  {
+    "revision": "1add4d7d3f2a3de828d3",
+    "url": "/connect4/static/js/2.42d39fca.chunk.js"
+  },
+  {
+    "revision": "20baa03cab693e438a71",
+    "url": "/connect4/static/js/main.871058a2.chunk.js"
+  },
+  {
+    "revision": "b3a84a2fc52093eda269",
+    "url": "/connect4/static/js/runtime-main.aafa7cef.js"
+  }
+]);
